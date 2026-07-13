c35e0b2e777cbe12e8b1c6fb2260f7d291337d1d
phamanhquanthcshongquang-png <phamanhquanthcshongquang@gmail.com>
2026-07-13 22:27:17 +0700

    bài 1

--- đề thi thử/đề 1/bài 1/code.py
+++ đề thi thử/đề 1/bài 1/code.py
@@ -1,3 +1,20 @@
-file = open('CDIV.INP')
-data = file.read()
-print(data)
+with open('CDIV.INP','r') as file:
+    lines = file.readlines()
+    inp = lines[1].strip()
+mang = list(map(int,inp.split()))
+gtln = max(mang)
+count = [0] * (gtln + 1)
+for i in mang:
+    count[i] += 1
+for g in range(gtln, 0, -1):
+    soboi = 0
+    for sobo in range(g, gtln + 1, g):
+        soboi += count[sobo]
+        if soboi >= 2:
+           with open('CDIV.OUT', 'w') as file2:
+               data = file2.write(str(g))
+           break
+    if soboi >= 2:
+        break
+    
+    
